53aa73447727bb6d9188488346f9ff4143667dd2
Andres Ortiz <141180065+AndresO7@users.noreply.github.com>
2025-03-10 00:50:42 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
       return [
         {
           source: '/api/:path*',
-          destination: 'https://2a52-2800-430-1203-56c7-bdc7-7941-6b2d-f77b.ngrok-free.app/api/:path*' 
+          destination: 'http://13.91.39.82/api/:path*' 
         }
       ];
     },
